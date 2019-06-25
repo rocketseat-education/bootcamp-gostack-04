69b8f85ababf5c8a8d54f4452823e58816c8dbd5
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-25 13:37:22 -0300

    5 - Importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import profile from './assets/profile.png';
+
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return <img width="200" src={profile} />
 }
 
 export default App;
