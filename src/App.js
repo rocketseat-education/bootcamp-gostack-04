342af783d941caad6eedc4ab0707bce3fa79c95c
Victor <aldroyd@gmail.com>
2019-07-01 12:32:16 -0300

    12 - Resultado do desafio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import TechList from './components/TechList';
+import Header from './components/Header';
+import PostList from './components/PostList';
 
 function App() {
-  return <TechList />
+  return (
+    <>
+      <Header />
+      <PostList />
+    </>
+  );
 }
 
 export default App;
